351b6d9d283d721905ffcacfe35b5ec5d1bf972c
Krishna Singh <singhks0054@gmail.com>
2022-04-27 11:50:46 +0530

    user model

--- model/User.js
+++ model/User.js
@@ -10,8 +10,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true,
-    unique: true,
-    uppercase: true
+    unique: true
   },
   password: {
     type: String,
